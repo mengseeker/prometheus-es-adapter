ca12afad3d4ad8157378549c2fab1355b789d10e
biao <biao.xing@nexttao.com>
2021-03-16 19:26:53 +0800

    init

--- pkg/elasticsearch/const.go
+++ pkg/elasticsearch/const.go
@@ -10,35 +10,37 @@ const indexCreate = `{
 
 const indexTemplate = `{
 	"index_patterns": ["{{.Alias}}-*"],
-	"settings": {
-		"number_of_shards": {{.Shards}},
-		"number_of_replicas": {{.Replicas}}
-	},
-	"mappings": {
-		"sample": {
-			"_source": {
-				"enabled": true
-			},
-			"properties": {
-				"timestamp": {
-					"type": "date",
-					"format": "strict_date_optional_time||epoch_millis"
+	"template": {
+		"settings": {
+			"number_of_shards": {{.Shards}},
+			"number_of_replicas": {{.Replicas}}
+		},
+		"mappings": {
+			"sample": {
+				"_source": {
+					"enabled": true
 				},
-				"value": {
-					"type": "double"
-				}
-			},
-			"dynamic_templates": [
-				{
-					"strings": {
-						"match_mapping_type": "string",
-						"path_match": "label.*",
-						"mapping": {
-							"type": "keyword"
+				"properties": {
+					"timestamp": {
+						"type": "date",
+						"format": "strict_date_optional_time||epoch_millis"
+					},
+					"value": {
+						"type": "double"
+					}
+				},
+				"dynamic_templates": [
+					{
+						"strings": {
+							"match_mapping_type": "string",
+							"path_match": "label.*",
+							"mapping": {
+								"type": "keyword"
+							}
 						}
 					}
-				}
-			]
+				]
+			}
 		}
 	}
 }`
